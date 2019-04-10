625597c031b3118b863ee3720d4b9cf4491c75c0
daormar <dortiz@prhlt.upv.es>
2019-04-10 16:27:21 +0200

    Add new tool.

---
+++ utils/panpipe_doc_mod.sh
@@ -0,0 +1,101 @@
+# *- bash -*
+
+# INCLUDE BASH LIBRARY
+. ${panpipe_bindir}/panpipe_lib || exit 1
+
+########
+print_desc()
+{
+    echo "panpipe_doc_mod generates documentation about a given module"
+    echo "type \"panpipe_mod_info --help\" to get usage information"
+}
+
+########
+usage()
+{
+    echo "panpipe_doc_mod           -m <string> [-s <string>]"
+    echo "                          [--showopts] [--help]"
+    echo ""
+    echo "-m <string>               Module file name"
+    echo "-s <string>               Step name whose information should be obtained"
+    echo "--showopts                Show option information"
+    echo "--help                    Display this help and exit"
+}
+
+########
+read_pars()
+{
+    m_given=0
+    s_given=0
+    showopts_given=0
+    while [ $# -ne 0 ]; do
+        case $1 in
+            "--help") usage
+                      exit 1
+                      ;;
+            "-m") shift
+                  if [ $# -ne 0 ]; then
+                      module_fname=$1
+                      m_given=1
+                  fi
+                  ;;
+            "-s") shift
+                  if [ $# -ne 0 ]; then
+                      given_stepname=$1
+                      s_given=1
+                  fi
+                  ;;
+            "--showopts") showopts_given=1
+                          ;;
+        esac
+        shift
+    done   
+}
+
+########
+check_pars()
+{
+    if [ ${m_given} -eq 0 ]; then
+        echo "Error! -m parameter not given!" >&2
+        exit 1
+    else
+        if [ ! -f ${module_fname} ]; then
+            echo "Error! module file does not exist" >&2 
+            exit 1
+        fi
+    fi
+}
+
+########
+get_step_doc_funcnames()
+{
+    declare -F | $AWK '{start=index($3,"_document"); if(start!=0 && start-1+length("_document")==length($3)) printf"%s\n",$3}'
+}
+
+########
+obtain_info_for_module()
+{
+    # Load module
+    source ${module_fname}
+
+    # Iterate over step documentation functions
+    while read step_doc_func; do
+        local stepname=${step_doc_func%"_document"}
+        document_step ${stepname} ${showopts_given}
+    done < <(get_step_doc_funcnames)
+}
+
+########
+
+if [ $# -eq 0 ]; then
+    print_desc
+    exit 1
+fi
+
+read_pars $@ || exit 1
+
+check_pars || exit 1
+
+obtain_info_for_module
+
+exit $?
