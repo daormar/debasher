687de92c86203d00348ed74661baef5241dae208
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2019-07-26 14:13:48 +0200

    Add -e option.

--- utils/reformat_pipe_status.py
+++ utils/reformat_pipe_status.py
@@ -13,9 +13,10 @@ def take_pars():
     values={}
     flags["f_given"]=False
     flags["l_given"]=False
+    flags["e_given"]=False
     
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"f:l:",["format=","fieldlen="])
+        opts, args = getopt.getopt(sys.argv[1:],"f:l:e:",["format=","fieldlen=","excl="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -30,6 +31,9 @@ def take_pars():
             elif opt in ("-l", "--fieldlen"):
                 values["fieldlen"] = int(arg)
                 flags["l_given"]=True
+            elif opt in ("-e", "--excl"):
+                values["excl"] = arg
+                flags["e_given"]=True
 
     return (flags,values)
 
@@ -44,12 +48,13 @@ def check_pars(flags,values):
 
 ##################################################
 def print_help():
-    print >> sys.stderr, "reformat_pipe_status -f <int> -l <int>"
+    print >> sys.stderr, "reformat_pipe_status -f <int> -l <int> [-e <string>]"
     print >> sys.stderr, ""
     print >> sys.stderr, "-f <int>             Output format:"
     print >> sys.stderr, "                     ",ROW_WITH_HEADER_FORMAT,"-> one row with header, plain text"
     print >> sys.stderr, "                     ",ROW_WO_HEADER_FORMAT,"-> one row without header, plain text"
     print >> sys.stderr, "-l <int>             Field length:"
+    print >> sys.stderr, "-e <string>          Comma-separated list of steps to be excluded"
 
 ##################################################
 def extract_step_info():
@@ -76,37 +81,51 @@ def norm_str_len(str,normlen):
     return result
 
 ##################################################
-def print_step_info(step_map,format,fieldlen):
+def print_step_info(step_map,excl_steps_set,format,fieldlen):
     if format==ROW_WITH_HEADER_FORMAT:
         header=""
         for step in step_map:
-            if header=="":
-                header=norm_str_len(step,fieldlen)
-            else:
-                header=header+" "+norm_str_len(step,fieldlen)
+            if step not in excl_steps_set:
+                if header=="":
+                    header=norm_str_len(step,fieldlen)
+                else:
+                    header=header+" "+norm_str_len(step,fieldlen)
         print header
 
         status=""
         for step in step_map:
-            if status=="":
-                status=norm_str_len(step_map[step],fieldlen)
-            else:
-                status=status+" "+norm_str_len(step_map[step],fieldlen)
+            if step not in excl_steps_set:
+                if status=="":
+                    status=norm_str_len(step_map[step],fieldlen)
+                else:
+                    status=status+" "+norm_str_len(step_map[step],fieldlen)
         print status
         
     elif format==ROW_WO_HEADER_FORMAT:
         status=""
         for step in step_map:
-            if status=="":
-                status=norm_str_len(step_map[step],fieldlen)
-            else:
-                status=status+" "+norm_str_len(step_map[step],fieldlen)
+            if step not in excl_steps_set:
+                if status=="":
+                    status=norm_str_len(step_map[step],fieldlen)
+                else:
+                    status=status+" "+norm_str_len(step_map[step],fieldlen)
         print status
         
 ##################################################
+def extract_excl_step_info(excl_steps):
+    fields=excl_steps.split(",")
+    excl_steps_set=set()
+    for step in fields:
+        excl_steps_set.add(step)
+    return excl_steps_set
+    
+##################################################
 def process_pars(flags,values):
     step_map=extract_step_info()
-    print_step_info(step_map,values["format"],values["fieldlen"])
+    excl_steps_set=set()
+    if flags["e_given"]:
+        excl_steps_set=extract_excl_step_info(values["excl"])
+    print_step_info(step_map,excl_steps_set,values["format"],values["fieldlen"])
     
 ##################################################
 def main(argv):
