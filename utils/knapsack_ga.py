bb50205afc80c1b5e45b9e6dbac15a0ced8afb2d
daormar <dortiz@prhlt.upv.es>
2020-05-12 20:33:02 +0200

    Add missing comments.

--- utils/knapsack_ga.py
+++ utils/knapsack_ga.py
@@ -16,6 +16,9 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 """
  
+# *- python -*
+
+# import modules
 import random, time
 
 ##############################################################################
