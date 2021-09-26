6dc5bf5cb136ec25a621fe790d15debf282ad098
daormar <daniel.ortiz.phd@gmail.com>
2021-09-27 00:54:00 +0200

    Improve code robustness.

--- utils/panpipe_lib.sh
+++ utils/panpipe_lib.sh
@@ -263,34 +263,10 @@ dirnames_are_equal()
 }
 
 ########
-expand_tilde_in_word()
-{
-    local word=$1
-
-    case "$word" in
-        "~/"*) echo "${HOME}/${word#"~/"}"
-               ;;
-        *) echo "$word"
-           ;;
-    esac
-}
-
-########
 expand_tildes()
 {
     local str=$1
-    if [ "${str}" != "" ]; then
-        local result=""
-        for w in $str; do
-            w=`expand_tilde_in_word $w`
-            if [ "$result" = "" ]; then
-                result="$w"
-            else
-                result="${result} ${w}"
-            fi
-        done
-        echo ${result}
-    fi
+    echo "${str//\~/$HOME}"
 }
 
 ########
