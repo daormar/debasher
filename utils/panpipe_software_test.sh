9c5cc37c5e7cd9b2dc09e78ff6bc1141e269c913
daormar <daniel.ortiz.phd@gmail.com>
2023-09-29 16:52:39 +0200

    Add new function.

--- utils/panpipe_software_test.sh
+++ utils/panpipe_software_test.sh
@@ -534,3 +534,17 @@ process_i()
     # Write value to file
     echo "$value" > "${outf}"
 }
+
+########
+panpipe_software_test_workflow()
+{
+    add_panpipe_process "process_a" "cpus=1 mem=32 time=00:01:00,00:02:00,00:03:00" "processdeps=none"
+    add_panpipe_process "process_b" "cpus=1 mem=32 time=00:01:00" "processdeps=none"
+    add_panpipe_process "process_c" "cpus=1 mem=32 time=00:01:00,00:02:00 throttle=2" "processdeps=none"
+    add_panpipe_process "process_d" "cpus=1 mem=32 time=00:01:00" "processdeps=none"
+    add_panpipe_process "process_e" "cpus=1 mem=32 time=00:01:00" "processdeps=after:process_d"
+    add_panpipe_process "process_f" "cpus=1 mem=32 time=00:01:00" "processdeps=none"
+    add_panpipe_process "process_g" "cpus=1 mem=32 time=00:01:00 throttle=4" "processdeps=aftercorr:process_c"
+    add_panpipe_process "process_h" "cpus=1 mem=32 time=00:01:00" "processdeps=afterok:process_a?afterok:process_f"
+    add_panpipe_process "process_i" "cpus=1 mem=32 time=00:01:00" "processdeps=afterok:process_b"
+}
