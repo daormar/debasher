1b1b4dbd332c0e6015cea488f2bcb6dc9527d2f8
daormar <daniel.ortiz.phd@gmail.com>
2023-11-20 20:00:35 +0100

    Revise variable names.

--- utils/panpipe_lib_sched_builtin.sh
+++ utils/panpipe_lib_sched_builtin.sh
@@ -128,9 +128,9 @@ get_elapsed_time_for_process_builtin()
 ########
 dyn_launch_builtin()
 {
-    local process_name=$1
+    local process_to_launch=$1
 
     # Execute process
     shift
-    "${process_name}" $@ || return 1
+    "${process_to_launch}" $@ || return 1
 }
