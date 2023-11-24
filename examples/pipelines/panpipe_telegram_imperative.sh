808af31bbbc47d6e0235cce1acd7349aefab21d5
daormar <daniel.ortiz.phd@gmail.com>
2023-11-24 13:08:22 +0100

    Revise code.

--- examples/pipelines/panpipe_telegram_imperative.sh
+++ examples/pipelines/panpipe_telegram_imperative.sh
@@ -16,6 +16,9 @@
 
 # *- bash -*
 
+# Module imports
+load_panpipe_module "panpipe_telegram_jobsteps"
+
 #############
 # CONSTANTS #
 #############
@@ -27,142 +30,12 @@
 ########
 panpipe_telegram_imperative_shared_dirs()
 {
-    define_shared_dir "data"
+    :
 }
 
-###################################
-# PIPELINE SOFTWARE TESTING STEPS #
-###################################
-
-########
-decomposer_document()
-{
-    step_description "Telegram Problem Decomposer module."
-}
-
-########
-decomposer_explain_cmdline_opts()
-{
-    # -f option
-    local description="File to be processed"
-    explain_cmdline_req_opt "-f" "<string>" "$description"
-}
-
-########
-decomposer_define_opts()
-{
-    # Initialize variables
-    local cmdline=$1
-    local stepspec=$2
-    local process_name=$3
-    local process_outdir=$4
-    local optlist=""
-
-    # -f option
-    define_cmdline_opt "$cmdline" "-f" optlist || return 1
-
-    # Get absolute name of shared directory
-    local abs_shrdir=$(get_absolute_shdirname "data")
-
-    # Define name of output file
-    local outf="${abs_shrdir}/words.txt"
-    define_opt "-outf" "${outf}" optlist || return 1
-
-    # Save option list
-    save_opt_list optlist
-}
-
-########
-decomposer()
-{
-    # Initialize variables
-    local file=$(read_opt_value_from_func_args "-f" "$@")
-    local outf=$(read_opt_value_from_func_args "-outf" "$@")
-
-    # Write string to FIFO
-    "${CAT}" "${file}" | "${AWK}" '{for(i=1;i<=NF;++i) print $i}' > "${outf}" ; pipe_fail || return 1
-}
-
-########
-recomposer_document()
-{
-    step_description "Telegram Problem Recomposer module."
-}
-
-########
-recomposer_explain_cmdline_opts()
-{
-    # -c option
-    local description="Line length in characters"
-    explain_cmdline_req_opt "-c" "<int>" "$description"
-}
-
-########
-recomposer_define_opts()
-{
-    # Initialize variables
-    local cmdline=$1
-    local stepspec=$2
-    local process_name=$3
-    local process_outdir=$4
-    local optlist=""
-
-    # -c option
-    define_cmdline_opt "$cmdline" "-c" optlist || return 1
-
-    # Get absolute name of shared directory
-    local abs_shrdir=$(get_absolute_shdirname "data")
-
-    # Define name of input file
-    local inf="${abs_shrdir}/words.txt"
-    define_opt "-inf" "${inf}" optlist || return 1
-
-    # Define name of output file
-    local outf="${process_outdir}/output.txt"
-    define_opt "-outf" "${outf}" optlist || return 1
-
-    # Save option list
-    save_opt_list optlist
-}
-
-########
-recompose()
-{
-    local char_lim=$1
-
-    "${AWK}" -v maxlen="${char_lim}" 'BEGIN{len=0}
-             {
-              for(i=1; i<=NF; ++i)
-              {
-               if(len + length($i) <= maxlen)
-               {
-                 printf"%s", $i
-                 len=len+length($i)
-                 if(len+1 <= maxlen)
-                  printf" "
-               }
-               else
-               {
-                 printf"\n%s",$i
-                 len=length($i)
-                 if(len+1 <= maxlen)
-                  printf" "
-               }
-              }
-             }'
-}
-
-########
-recomposer()
-{
-    # Initialize variables
-    local outf=$(read_opt_value_from_func_args "-outf" "$@")
-    local char_lim=$(read_opt_value_from_func_args "-c" "$@")
-    local inf=$(read_opt_value_from_func_args "-inf" "$@")
-
-    # Write string to FIFO
-    "${CAT}" "${inf}" | recompose "${char_lim}" > "${outf}" ; pipe_fail || return 1
-}
+######################
+# PIPELINE PROCESSES #
+######################
 
 ########
 telegram_document()
