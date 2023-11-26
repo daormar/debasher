3e187061c4dbe5250b615b8291baf5e3d3d47dcb
daormar <daniel.ortiz.phd@gmail.com>
2023-11-26 21:26:50 +0100

    Add new file.

---
+++ examples/pipelines/panpipe_conda_example.sh
@@ -0,0 +1,90 @@
+# PanPipe package
+# Copyright (C) 2019,2020 Daniel Ortiz-Mart\'inez
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+# *- bash -*
+
+#############
+# CONSTANTS #
+#############
+
+#################
+# CFG FUNCTIONS #
+#################
+
+#######################################
+# PIPELINE SOFTWARE TESTING PROCESSES #
+#######################################
+
+########
+conda_example_document()
+{
+    process_description "Prints Python version to file \`python_ver.txt\`."
+}
+
+########
+conda_example_explain_cmdline_opts()
+{
+    :
+}
+
+########
+conda_example_define_opts()
+{
+    # Initialize variables
+    local cmdline=$1
+    local process_spec=$2
+    local process_name=$3
+    local process_outdir=$4
+    local optlist=""
+
+    # Define name of output file
+    define_opt "-outf" "${process_outdir}/python_ver.txt" optlist || return 1
+
+    # Save option list
+    save_opt_list optlist
+}
+
+########
+conda_example()
+{
+    # Initialize variables
+    local outf=$(read_opt_value_from_func_args "-outf" "$@")
+
+    # Activate conda environment
+    conda activate py27 || return 1
+
+    # Write python version to file
+    python --version > "${outf}" 2>&1 || return 1
+
+    # Deactivate conda environment
+    conda deactivate
+}
+
+########
+conda_example_conda_envs()
+{
+    define_conda_env py27 py27.yml
+}
+
+##################################
+# PIPELINE DEFINED BY THE MODULE #
+##################################
+
+########
+panpipe_conda_example_pipeline()
+{
+    add_panpipe_process "conda_example" "cpus=1 mem=32 time=00:01:00"
+}
---
+++ examples/pipelines/panpipe_docker_example.sh
@@ -0,0 +1,78 @@
+# PanPipe package
+# Copyright (C) 2019,2020 Daniel Ortiz-Mart\'inez
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+# *- bash -*
+
+#############
+# CONSTANTS #
+#############
+
+#################
+# CFG FUNCTIONS #
+#################
+
+#######################################
+# PIPELINE SOFTWARE TESTING PROCESSES #
+#######################################
+
+########
+docker_example_explain_cmdline_opts()
+{
+    :
+}
+
+########
+docker_example_define_opts()
+{
+    # Initialize variables
+    local cmdline=$1
+    local process_spec=$2
+    local process_name=$3
+    local process_outdir=$4
+    local optlist=""
+
+    # Define name of output file
+    define_opt "-outf" "${process_outdir}/hello_world.txt" optlist || return 1
+
+    # Save option list
+    save_opt_list optlist
+}
+
+########
+docker_example()
+{
+    # Initialize variables
+    local outf=$(read_opt_value_from_func_args "-outf" "$@")
+
+    # Write python version to file
+    "${DOCKER}" run hello-world > "${outf}" 2>&1 || return 1
+}
+
+########
+docker_example_docker_imgs()
+{
+    pull_docker_img "library/hello-world"
+}
+
+##################################
+# PIPELINE DEFINED BY THE MODULE #
+##################################
+
+########
+panpipe_docker_example_pipeline()
+{
+    add_panpipe_process "docker_example" "cpus=1 mem=32 time=00:01:00"
+}
