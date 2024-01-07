593ffd48c86f595384e4da8359b4c8f0d4c89e98
daormar <daniel.ortiz.phd@gmail.com>
2024-01-07 14:09:31 +0100

    Refactor code.

--- rtdocs/generate_api_files.sh
+++ rtdocs/generate_api_files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Generate process options API file
+proc_opts_symbols=("define_opt()")
 rm -f rtdocs/source/api_proc_opts_doc.md
-for name in "define_opt()"; do
-    sh rtdocs/tomdoc.sh -m -a Public -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_opts_doc.md
+for name in "${proc_opts_symbols[@]}"; do
+    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_opts_doc.md
 done
