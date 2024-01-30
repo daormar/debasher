d87c1d57305e3e5923786fe63054cce065206029
daormar <daniel.ortiz.phd@gmail.com>
2024-01-30 14:53:14 +0100

    Add upvar function.

--- utils/debasher_lib_utils.sh
+++ utils/debasher_lib_utils.sh
@@ -33,6 +33,26 @@ version_to_number()
 }
 
 ########
+# Assign variable one scope above the caller
+# Usage: local "$1" && upvar $1 "value(s)"
+# Param: $1  Variable name to assign value to
+# Param: $*  Value(s) to assign.  If multiple values, an array is
+#            assigned, otherwise a single value is assigned.
+# NOTE: For assigning multiple variables, use 'upvars'.  Do NOT
+#       use multiple 'upvar' calls, since one 'upvar' call might
+#       reassign a variable to be used by another 'upvar' call.
+# See: http://fvue.nl/wiki/Bash:_Passing_variables_by_reference
+upvar() {
+    if unset -v "$1"; then           # Unset & validate varname
+        if (( $# == 2 )); then
+            eval $1=\"\$2\"          # Return single value
+        else
+            eval $1=\(\"\${@:2}\"\)  # Return array
+        fi
+    fi
+}
+
+########
 pipe_fail()
 {
     # test if there is at least one command to exit with a non-zero status
