20672ca281d04549dfe7df1cd01e00b9102be5fe
daormar <daniel.ortiz.phd@gmail.com>
2024-04-23 22:31:22 +0200

    Enable css files.

--- rtdocs/source/conf.py
+++ rtdocs/source/conf.py
@@ -28,9 +28,15 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_static_path = ['_static']
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
+html_css_files = [
+    'css/styles.css',
+]
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
