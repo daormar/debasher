f76a630ed8e37cec586daa678ab1cd93e523a42e
daormar <daniel.ortiz.phd@gmail.com>
2024-05-25 15:48:38 +0200

    Update api files generation.

--- rtdocs/generate_api_files.sh
+++ rtdocs/generate_api_files.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Generate program command line options API file
-cmdline_opts_symbols=("explain_cmdline_opt()")
-rm -f rtdocs/source/api_cmdline_opts_doc.md
-for name in "${cmdline_opts_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_cmdline_opts_doc.md
-done
-
 # Generate process options API file
 proc_opts_symbols=("define_opt()")
 rm -f rtdocs/source/api_proc_opts_doc.md
@@ -14,18 +7,18 @@ for name in "${proc_opts_symbols[@]}"; do
     sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_opts_doc.md
 done
 
-# Generate process execution API file
-proc_exec_symbols=("read_opt_value_from_func_args()")
-rm -f rtdocs/source/api_proc_exec_doc.md
-for name in "${proc_exec_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_exec_doc.md
+# Generate process implementation API file
+proc_impl_symbols=("read_opt_value_from_func_args()")
+rm -f rtdocs/source/api_proc_impl_doc.md
+for name in "${proc_impl_symbols[@]}"; do
+    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_impl_doc.md
 done
 
-# Generate program definition API file
-prog_def_symbols=("add_debasher_process()")
-rm -f rtdocs/source/api_prog_def_doc.md
-for name in "${prog_def_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_programs.sh >> rtdocs/source/api_prog_def_doc.md
+# Generate program command line options API file
+cmdline_opts_symbols=("explain_cmdline_opt()")
+rm -f rtdocs/source/api_cmdline_opts_doc.md
+for name in "${cmdline_opts_symbols[@]}"; do
+    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_cmdline_opts_doc.md
 done
 
 # Generate module configuration API file
@@ -33,4 +26,11 @@ mod_conf_symbols=("load_debasher_module()")
 rm -f rtdocs/source/api_mod_conf_doc.md
 for name in "${mod_conf_symbols[@]}"; do
     sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_programs.sh >> rtdocs/source/api_mod_conf_doc.md
+done
+
+# Generate program definition API file
+prog_def_symbols=("add_debasher_process()")
+rm -f rtdocs/source/api_prog_def_doc.md
+for name in "${prog_def_symbols[@]}"; do
+    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_programs.sh >> rtdocs/source/api_prog_def_doc.md
 done
