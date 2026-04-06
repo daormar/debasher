837a11b07886e905424bca9c8c89d1f87864513b
daormar <daniel.ortiz.phd@gmail.com>
2026-04-06 22:56:35 +0200

    Add new file.

---
+++ examples/programs/debasher_host_workflow_force.sh
@@ -0,0 +1,31 @@
+# DeBasher package
+# Copyright (C) 2019-2024 Daniel Ortiz-Mart\'inez
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+# *- bash -*
+
+# Load modules
+load_debasher_module "debasher_host_workflow"
+
+#################################
+# PROGRAM DEFINED BY THE MODULE #
+#################################
+
+########
+debasher_host_workflow_force_program()
+{
+    add_debasher_process "host1" "cpus=1 mem=32 time=00:10:00 throttle=64" "force=yes"
+    add_debasher_process "host2" "cpus=1 mem=32 time=00:10:00 throttle=64"
+}
