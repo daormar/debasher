30c353a0b585259fbc22010f10181b2b3ece86b9
daormar <daniel.ortiz.phd@gmail.com>
2026-07-24 11:13:07 +0200

    Revise and extend code.

--- utils/debasher_lib_sched_topo_sort.sh
+++ utils/debasher_lib_sched_topo_sort.sh
@@ -19,13 +19,12 @@
 ###################################################
 
 ########
-#!/usr/bin/env bash
-
-# topo_sort <assoc_array_name_deps> <result_array_name>
-debasher::topo_sort()
+# debasher::_topo_sort <dependency_separator> <assoc_array_name_deps> <result_array_name>
+debasher::_topo_sort()
 {
-    local -n _deps=$1      # reference to the associative array of dependencies
-    local -n _result=$2    # reference to the output array (final order)
+    local dep_sep=$1       # Character used as dependency separator
+    local -n _deps=$2      # reference to the associative array of dependencies
+    local -n _result=$3    # reference to the output array (final order)
 
     local -A _state        # 0=unvisited, 1=in progress, 2=done
     local _order=()
@@ -46,11 +45,12 @@ debasher::topo_sort()
         _state[$node]=1
 
         local depstr=${_deps[$node]}
-        if [[ -n $depstr && $depstr != "none" ]]; then
-            local IFS=','
+        if [[ -n $depstr && $depstr != "${DEBASHER_NONE_PROCESSDEP_TYPE}" ]]; then
+            local -a deps_array
+            IFS="${dep_sep}" read -ra deps_array <<< "${depstr}"
             local dep
-            for dep in $depstr; do
-                if [[ -z ${_deps[$dep]+x} ]]; then
+            for dep in "${deps_array[@]}"; do
+                if [[ ! -v _deps[$dep] ]]; then
                     echo "Error: '$dep' (dependency of '$node') is not defined" >&2
                     exit 1
                 fi
@@ -68,4 +68,10 @@ debasher::topo_sort()
     done
 
     _result=("${_order[@]}")
+}
+
+########
+debasher::_topo_sort_processes()
+{
+    debasher::_topo_sort "${DEBASHER_PROCESSDEPS_SEP_COMMA}" DEBASHER_PROCESS_DEPENDENCIES_SIMPLIFIED DEBASHER_PROGRAM_PROCESSES_TOPO_SORT
 }
