428fb86e24823d5f8f43dc2e42b700df7128c5fb
daormar <daniel.ortiz.phd@gmail.com>
2026-08-27 14:46:58 +0200

    Correct directory name.

--- rtdocs/generate_api_files.sh
+++ rtdocs/generate_api_files.sh
@@ -4,33 +4,33 @@
 proc_opts_symbols=("define_opt()" "define_cmdline_opt()" "define_opt_from_proc_out()" "save_opt_list()")
 rm -f rtdocs/source/api_proc_opts_doc.md
 for name in "${proc_opts_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_opts_doc.md
+    sh rtdocs/tomdoc.sh -m -s "${name}" engine/debasher_lib_opts.sh >> rtdocs/source/api_proc_opts_doc.md
 done
 
 # Generate process implementation API file
 proc_impl_symbols=("read_opt_value_from_func_args()")
 rm -f rtdocs/source/api_proc_impl_doc.md
 for name in "${proc_impl_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_proc_impl_doc.md
+    sh rtdocs/tomdoc.sh -m -s "${name}" engine/debasher_lib_opts.sh >> rtdocs/source/api_proc_impl_doc.md
 done
 
 # Generate option characterization API file
 cmdline_opts_symbols=("explain_cmdline_opt()" "explain_opt() opt_is_cmdline()")
 rm -f rtdocs/source/api_characterize_opts_doc.md
 for name in "${cmdline_opts_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_opts.sh >> rtdocs/source/api_characterize_opts_doc.md
+    sh rtdocs/tomdoc.sh -m -s "${name}" engine/debasher_lib_opts.sh >> rtdocs/source/api_characterize_opts_doc.md
 done
 
 # Generate module configuration API file
 mod_conf_symbols=("load_debasher_module()" "module_description()")
 rm -f rtdocs/source/api_mod_conf_doc.md
 for name in "${mod_conf_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_modules.sh >> rtdocs/source/api_mod_conf_doc.md
+    sh rtdocs/tomdoc.sh -m -s "${name}" engine/debasher_lib_modules.sh >> rtdocs/source/api_mod_conf_doc.md
 done
 
 # Generate program definition API file
 prog_def_symbols=("add_debasher_process()")
 rm -f rtdocs/source/api_prog_def_doc.md
 for name in "${prog_def_symbols[@]}"; do
-    sh rtdocs/tomdoc.sh -m -s "${name}" utils/debasher_lib_programs.sh >> rtdocs/source/api_prog_def_doc.md
+    sh rtdocs/tomdoc.sh -m -s "${name}" engine/debasher_lib_programs.sh >> rtdocs/source/api_prog_def_doc.md
 done
