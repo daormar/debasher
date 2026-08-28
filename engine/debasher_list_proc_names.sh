5fdc46f90ab1ab16d62d3ecf3c0af1c7a608a557
daormar <daniel.ortiz.phd@gmail.com>
2026-08-28 11:12:50 +0200

    Add new file.

---
+++ engine/debasher_list_proc_names.sh
@@ -0,0 +1,30 @@
+# DeBasher package
+# Copyright (C) 2019-2026 Daniel Ortiz-Mart\'inez
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; If not, see <http://www.gnu.org/licenses/>.
+
+# *- bash -*
+
+# INCLUDE BASH LIBRARY
+. "${debasher_pkglibdir}"/debasher_lib || exit 1
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: debasher_list_proc_names <file>"
+    exit 1
+fi
+
+file=$1
+source "$file"
+
+debasher::list_proc_names
