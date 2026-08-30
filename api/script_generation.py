feaf9a7ea7880f8b464db573c81d265936347290
daormar <daniel.ortiz.phd@gmail.com>
2026-08-30 17:45:41 +0200

    Implement Bash script generation for process documentation and options

--- api/script_generation.py
+++ api/script_generation.py
@@ -42,24 +42,87 @@ def _add_preamble(preamble):
 
 def _add_document_func(process):
     lines = [f"{process.name}{PROCESS_METHOD_DOCUMENT_SUFFIX}()", "{"]
+    if process.description:
+        lines.append(f'{INDENT}debasher::process_description "{process.description}"')
+    else:
+        lines.append(INDENT + ":")
     lines.append("}")
     return lines
 
 
 def _add_explain_opts_func(process):
     lines = [f"{process.name}{PROCESS_METHOD_EXPLAIN_OPTS_SUFFIX}()", "{"]
+    if process.options:
+        for option in process.options:
+            lines.append(f'{INDENT}debasher::explain_opt "{option.label}" "{option.dataType}" "{option.description}"')
+    else:
+        lines.append(INDENT + ":")
     lines.append("}")
     return lines
 
 
 def _add_identify_cmdline_opts_func(process):
     lines = [f"{process.name}{PROCESS_METHOD_IDENTIFY_CMDLINE_OPTS_SUFFIX}()", "{"]
+    if process.options:
+        cmdline_option_found = False
+        for option in process.options:
+            if option.commandLine:
+                if option.mandatory:
+                    lines.append(f'{INDENT}debasher::opt_is_cmdline "{option.label}"')
+                else:
+                    lines.append(f'{INDENT}debasher::opt_is_non_mandatory_cmdline "{option.label}"')
+                cmdline_option_found = True
+        if not cmdline_option_found:
+            lines.append(INDENT + ":")
+    else:
+        lines.append(INDENT + ":")
     lines.append("}")
     return lines
 
 
+def _define_opts_func_header():
+    lines = []
+    lines.append(INDENT + "# Initialize variables")
+    lines.append(INDENT + "local cmdline=$1")
+    lines.append(INDENT + "local process_spec=$2")
+    lines.append(INDENT + "local process_name=$3")
+    lines.append(INDENT + "local process_outdir=$4")
+    lines.append(INDENT + "local optlist=\"\"")
+    return lines
+
+
+def _define_opts_func_foot():
+    lines = []
+    lines.append(INDENT + "save_opt_list optlist")
+    return lines
+
+def _opt_is_connected_to_proc(option):
+    return option.value.startswith("[") and option.value.endswith("]")
+
+
+def _get_process_plus_opt(option):
+    return tuple(option.value[1:-1].split(";"))
+
+
 def _add_define_opts_func(process):
     lines = [f"{process.name}{PROCESS_METHOD_DEFINE_OPTS_SUFFIX}()", "{"]
+    lines.extend(_define_opts_func_header())
+    lines.append("")
+    if process.options:
+        for option in process.options:
+            if option.commandLine:
+                if option.mandatory:
+                    lines.append(f'{INDENT}debasher::define_cmdline_opt "${{cmdline}}" "{option.label}" optlist || return 1')
+                else:
+                    lines.append(f'{INDENT}debasher::define_cmdline_opt_if_given "${{cmdline}}" "{option.label}" optlist || return 1')
+            else:
+                if _opt_is_connected_to_proc(option):
+                    conn_proc, conn_opt = _get_process_plus_opt(option)
+                    lines.append(f'{INDENT}debasher::define_opt_from_proc_out "{option.label}" "{conn_proc}" "{conn_opt}" optlist || return 1')
+                else:
+                    lines.append(f'{INDENT}debasher::define_opt "{option.label}" "{option.value}" optlist || return 1')
+    lines.append("")
+    lines.extend(_define_opts_func_foot())
     lines.append("}")
     return lines
 
@@ -67,6 +130,9 @@ def _add_define_opts_func(process):
 def _add_opts_handler(process):
     if process.optionsHandler.mode == "standard":
         return _add_define_opts_func(process)
+    raise NotImplementedError(
+        f'Options handler mode "{process.optionsHandler.mode}" is not implemented yet'
+    )
 
 
 def _add_exec_func(process):
