88ff85d818f604f1e3006cfbc95a82a73338bcbe
daormar <daniel.ortiz.phd@gmail.com>
2026-09-15 11:42:04 +0200

    Declare -infile in array_reader's explain_opts

--- data/programs/debasher_array_example.sh
+++ data/programs/debasher_array_example.sh
@@ -145,6 +145,10 @@ array_reader_explain_opts()
     local description="id of reader"
     explain_opt "-id" "<int>" "$description"
 
+    # -infile option
+    local description="input file"
+    explain_opt "-infile" "<file>" "$description"
+
     # -outdir option
     local description="output directory of reader"
     explain_opt "-outdir" "<file>" "$description"
